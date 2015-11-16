bce33c6e200d60b17c6557d9c9928edb547814de
Illyoung Choi <iychoi@email.arizona.edu>
2015-11-16 02:14:44 -0700

    Add sample json conf Change library path

--- testStargate.sh
+++ testStargate.sh
@@ -1,3 +1,3 @@
 echo "Running Stargate <- $1"
-java -cp dist/lib/*:dist/Stargate.jar edu.arizona.cs.stargate.service.test.StargateServiceTest $1
+java -cp libs/*:dist/Stargate.jar edu.arizona.cs.stargate.service.test.StargateServiceTest $1
 
