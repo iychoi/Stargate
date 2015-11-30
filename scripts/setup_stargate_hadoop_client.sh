edb017a2215b5290d1496d2342490df32ca8356e
Illyoung Choi <iychoi@email.arizona.edu>
2015-11-29 22:09:36 -0700

    Remove build

--- scripts/setup_stargate_hadoop_client.sh
+++ scripts/setup_stargate_hadoop_client.sh
@@ -5,12 +5,10 @@ curdir=$PWD
 cd ..
 
 cd stargate-commons
-ant
 cp dist/stargate-commons.jar ~/hadoop/lib
 cd ..
 
 cd stargate-client-hdfs
-ant
 cp dist/stargate-client-hdfs.jar ~/hadoop/lib
 cd ..
 
