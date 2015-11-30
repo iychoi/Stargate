b9597f518d84296fceb0d828ef222ce10cca255c
Illyoung Choi <iychoi@email.arizona.edu>
2015-11-29 23:30:57 -0700

    Add more log

--- stargate-server/src/stargate/drivers/sourcefs/hdfs/HDFSSourceFileSystemDriver.java
+++ stargate-server/src/stargate/drivers/sourcefs/hdfs/HDFSSourceFileSystemDriver.java
@@ -84,6 +84,7 @@ public class HDFSSourceFileSystemDriver extends ASourceFileSystemDriver {
         this.hadoopConfig = new Configuration();
         Path rootPath = this.config.getRootPath();
         String hdfsRoot = this.hadoopConfig.get("fs.default.name");
+        LOG.info("hdfs root - " + hdfsRoot);
         this.rootPath = new Path(hdfsRoot, rootPath);
         LOG.info("creating a filesystem for " + this.rootPath);
         this.filesystem = this.rootPath.getFileSystem(this.hadoopConfig);
